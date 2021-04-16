6522a00b1d1ba05f28a517151ee69082a33eec54
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-04-16 15:12:04 +0800

    Create CardInsertController.java

---
+++ src/main/java/controller/CardInsertController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import bean.Card;
+import service.CardService;
+
+public class CardInsertController implements Controller {
+	
+	private CardService cardService;
+
+	public CardService getCardService() {
+		return cardService;
+	}
+
+	public void setCardService(CardService cardService) {
+		this.cardService = cardService;
+	}
+
+
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		Card card = new Card();
+		card.setName(request.getParameter("name"));
+		card.setUserId(Long.parseLong(request.getParameter("userId")));
+		cardService.insertCard(card);
+		
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("../card");
+		mv.addObject(card);
+		return mv;
+	}
+
+}
