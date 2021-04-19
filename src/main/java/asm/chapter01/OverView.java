cd01b548e69ccda77811829102c55d0241193472
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-04-19 20:18:43 +0800

    Create OverView.java

---
+++ src/main/java/asm/chapter01/OverView.java
@@ -0,0 +1,15 @@
+package asm.chapter01;
+
+/**
+ * There are two kind API: event and tree
+ * 那里有两种类型API:事件和树
+ *
+ * event like SAX
+ * 事件跟SAX很相似
+ *
+ * tree like DOM
+ * tree跟DOM很相似
+ *
+ */
+public class OverView {
+}
