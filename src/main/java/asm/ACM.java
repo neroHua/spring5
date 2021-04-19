59b076587c3948c01a232eeee7a218dde4520423
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-04-19 20:18:15 +0800

    Create ACM.java

---
+++ src/main/java/asm/ACM.java
@@ -0,0 +1,23 @@
+package asm;
+
+/**
+ * this package is used to learn acm 5.2, including using and source code
+ * 这个包主要用来学习acm5.2, 包含使用和源代码
+ *
+ * the guide I learned is asm4-guide.pdf: https://asm.ow2.io/asm4-guide.pdf
+ * 我学习的引导书是asm4-guide.pdf：https://asm.ow2.io/asm4-guide.pdf
+ *
+ * two key command:
+ * 2个重要的指令
+ *
+ * 1. compile java file to class file with detailed debug information:
+ * 1. 把java源文件编译成带有调试信息的class文件
+ * javac -g xxxx.java
+ *
+ * 2. transfer class file from binary code to well formed and easy read assembly code
+ * 2. 把class文件从2进制代码转换成易于阅读且带有格式的汇编diam
+ * javac -v xxxx.class
+ *
+ */
+public class ACM {
+}
