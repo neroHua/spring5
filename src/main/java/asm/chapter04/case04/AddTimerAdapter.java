c897bff1790ab3b704c36319d8a6b8457cb2a700
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-04-24 15:18:24 +0800

    Create AddTimerAdapter.java

---
+++ src/main/java/asm/chapter04/case04/AddTimerAdapter.java
@@ -0,0 +1,45 @@
+package asm.chapter04.case04;
+
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.MethodVisitor;
+
+import static org.objectweb.asm.Opcodes.*;
+
+public class AddTimerAdapter extends ClassVisitor {
+
+    private String owner;
+    private boolean isInterface;
+
+    public AddTimerAdapter(ClassVisitor cv) {
+        super(ASM5, cv);
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        cv.visit(version, access, name, signature, superName, interfaces);
+        owner = name;
+        isInterface = (access & ACC_INTERFACE) != 0;
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        MethodVisitor mv = cv.visitMethod(access, name, desc, signature, exceptions);
+        if (!isInterface && mv != null && !name.equals("<init>")) {
+            mv = new AddTimerMethodAdapter(mv, owner);
+        }
+        return mv;
+    }
+
+    @Override
+    public void visitEnd() {
+        if (!isInterface) {
+            FieldVisitor fv = cv.visitField(ACC_PUBLIC + ACC_STATIC, "timer", "J", null, null);
+            if (fv != null) {
+                fv.visitEnd();
+            }
+        }
+        cv.visitEnd();
+    }
+
+}
