17eb00c222693c52cb44dffd377bf0f4ba98a770
滑德友 <38644203+neroHua@users.noreply.github.com>
2021-04-24 15:14:38 +0800

    Create ASMifierUse.java

---
+++ src/main/java/asm/chapter03/case04/ASMifierUse.java
@@ -0,0 +1,8 @@
+package asm.chapter03.case04;
+
+/**
+ * java -classpath D:\mavenLocalRepository\org\ow2\asm\asm-all\5.2\asm-all-5.2.jar org.objectweb.asm.util.ASMifier  java.lang.Runnable
+ * java -classpath D:\mavenLocalRepository\org\ow2\asm\asm-all\5.2\asm-all-5.2.jar org.objectweb.asm.util.ASMifier  TestClass.class
+ */
+public class ASMifierUse {
+}
